7f48f2aecfcc34026ba08e114864565437e59ad0
zyjy33 <1040135865@qq.com>
2018-04-20 10:12:35 +0800

    主页面，准备改写网络请求

--- app/src/main/java/com/yunsen/enjoy/model/ResponseBase.java
+++ app/src/main/java/com/yunsen/enjoy/model/ResponseBase.java
@@ -5,5 +5,85 @@ package com.yunsen.enjoy.model;
  */
 
 public class ResponseBase {
+    private String status;
+    private String code;
+    private String info;
+    private int record;
+    private String redirect;
+    private int timer;
+    private String callback;
+    private String datetime;
+    private String timestamp;
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getRecord() {
+        return record;
+    }
+
+    public void setRecord(int record) {
+        this.record = record;
+    }
+
+    public String getRedirect() {
+        return redirect;
+    }
+
+    public void setRedirect(String redirect) {
+        this.redirect = redirect;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public String getCallback() {
+        return callback;
+    }
+
+    public void setCallback(String callback) {
+        this.callback = callback;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
