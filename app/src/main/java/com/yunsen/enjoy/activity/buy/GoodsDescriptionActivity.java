ebbd80ac75d0ee5ffeefc8b76f2f9517307fcbc6
zyjy33 <1040135865@qq.com>
2018-05-12 18:45:04 +0800

    易客 提交部分购买页面的布局

---
+++ app/src/main/java/com/yunsen/enjoy/activity/buy/GoodsDescriptionActivity.java
@@ -0,0 +1,32 @@
+package com.yunsen.enjoy.activity.buy;
+
+import android.os.Bundle;
+
+import com.yunsen.enjoy.R;
+import com.yunsen.enjoy.activity.BaseFragmentActivity;
+
+/**
+ * Created by Administrator on 2018/5/12.
+ */
+
+public class GoodsDescriptionActivity extends BaseFragmentActivity {
+    @Override
+    public int getLayout() {
+        return R.layout.goods_description_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
